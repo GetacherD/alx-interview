4973712e9baeef9d997919056cdaf5874e521421
GetacherD <getacher.d@gmail.com>
2023-01-24 14:12:41 +0300

    use built in

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,8 +7,7 @@ Validate UTF-8
 def validUTF8(data):
     """ validate UTF-8"""
     try:
-        bits = [i & 255 for i in data]
-        bytes(bits).decode("UTF-8")
+        bytes(data).decode("UTF-8")
         return True
     except Exception:
         return False
