cde65f8eaeeba114906ca07948b9f18b59cf6281
GetacherD <getacher.d@gmail.com>
2023-02-07 14:30:38 +0300

    asnyc request for im order acces of characters of movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,41 @@
 #!/usr/bin/node
 const req = require('request');
 const id = process.argv[2];
-req('https://swapi-api.alx-tools.com/api/films/' + String(id),
-  function (err, resp, body) {
-    if (!err) {
-      const bdy = JSON.parse(body);
-      const chars = bdy.characters;
-      if (chars) {
-        for (const chr of chars) {
-          req(chr, function (err, resp, body) {
-            if (!err) {
-              console.log(JSON.parse(body).name);
-            }
-          });
-        }
-      }
+
+const filmAsync = function (id) {
+  return new Promise(
+    function (resolve, reject) {
+      req('https://swapi-api.alx-tools.com/api/films/' + id,
+        function (err, resp, body) {
+          if (!err) {
+            resolve(JSON.parse(body));
+          }
+        });
     }
+  );
+};
+const charAsync = function (url) {
+  return new Promise(
+    function (resolve, reject) {
+      req(url,
+        function (err, resp, body) {
+          if (!err) {
+            resolve(JSON.parse(body));
+          }
+        });
+    }
+  );
+};
+async function getAllCharacters (id) {
+  try {
+    const movie = await filmAsync(id);
+    const characters = await movie.characters;
+    for (const character of characters) {
+      const char = await charAsync(character);
+      console.log(char.name);
+    }
+  } catch {
+
   }
-);
+}
+getAllCharacters(id);
