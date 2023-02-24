dc9a4ee0eff5e7729a719ebf1053c6ad7a2b6e8a
Getacher  Demisse <GetacherD@ethiopianairlines.com>
2023-02-24 22:52:23 +0300

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,26 +4,30 @@ Make changes
 """
 
 
+stack = []
+
+def solve(ls, total, i):
+    """ helper solve function """
+    global  stack
+    if total == 0:
+        return
+    if i >= len(ls):
+        stack =[]
+        return
+    if total > 0:
+        stack.append(ls[i])
+        total -= ls[i]
+        solve(ls, total, i)
+    elif total < 0:
+        stack.pop()
+        total += ls[i]
+        i += 1
+        solve(ls, total, i)
+
+
 def makeChange(coins, total):
     """ make changes """
-    if total <= 0:
-        return 0
-
-    def combSum(candidates, target, index, cur, res):
-        """ helper function """
-        if target <= 0:
-            if target == 0:
-                res.append(cur[::])
-            return
-        if res:
-            return
-        for i in range(index, len(candidates)):
-            val = candidates[i]
-            cur.append(val)
-            combSum(candidates, target - val, i, cur, res)
-            last = cur.pop()
-        return res
-    res = combSum(sorted(list(set(coins)), reverse=True), total, 0, [], [])
-    if res:
-        return len(res[0])
-    return -1
+    global stack
+    ls = sorted(list(set(ls)), reverse=True)
+    solve(ls, total, 0)
+    return  len(stack)
